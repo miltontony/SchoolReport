b43c9b64ec250bf9918104aa95d0d140b0cd5640
Milton Madanda <milton@praekeltfoundation.org>
2012-08-04 15:17:47 +0200

    hello world

--- schoolreport/urls.py
+++ schoolreport/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'schoolreport.views.home', name='home'),
+    url(r'^$', 'schoolreport.views.home', name='home'),
     # url(r'^schoolreport/', include('schoolreport.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
