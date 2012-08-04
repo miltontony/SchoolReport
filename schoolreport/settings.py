a9ee6c91a3712e2e5bebd091704a90db74fa04e9
Milton Madanda <milton@praekeltfoundation.org>
2012-08-04 15:09:02 +0200

    cleaned up a bit

--- schoolreport/settings.py
+++ schoolreport/settings.py
@@ -116,10 +116,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'schoolreport',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    #'django.contrib.admin',
 )
 
 # A sample logging configuration. The only tangible logging
