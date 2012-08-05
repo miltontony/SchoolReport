5e04cec8126700ad68ce9c95992f42c680f3a09d
j-norwood-young <jason@10layer.com>
2012-08-05 10:37:08 +0200

    School model

--- schoolreport/models.py
+++ schoolreport/models.py
@@ -7,6 +7,35 @@ class UserProfile(models.Model):
     user = models.OneToOneField('auth.User')
     school = models.TextField(default='')
 
+class SchoolStatus(models.Model):
+	name = models.CharField(max_length = 10)
+	
+class SchoolType(models.Model):
+	name = models.CharField(max_length = 20)
+	
+class Municipality(models.Model):
+	name = models.CharField(max_length = 100)
+	
+class District(models.Model):
+	name = models.CharField(max_length = 100)
+
+class School(models.Model):
+	emis = models.IntegerField(unique = True)
+	school_status = models.OneToOneField(SchoolStatus)
+	name = models.CharField(max_length=100)
+	school_type = models.OneToOneField(SchoolType)
+	section21 = models.BooleanField()
+	fee = models.IntegerField()
+	municipality = models.OneToOneField(Municipality)
+	telephone = models.CharField(max_length = 30)
+	longitude = models.DecimalField(max_digits=10, decimal_places=3)
+	latitude = models.DecimalField(max_digits=10, decimal_places=3)
+	language = models.CharField(max_length = 20)
+	principal = models.CharField(max_length = 30)
+	physical_address = models.CharField(max_length = 255)
+	district = models.OneToOneField(District)
+	
+
 
 def create_profile(sender, instance, created, **kwargs):
     if created:
