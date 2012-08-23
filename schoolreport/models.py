bb2f56b693c91d90a9b34fe34cf992656bb390d5
j-norwood-young <jason@10layer.com>
2012-08-24 01:52:04 +0200

    Updated models

--- schoolreport/models.py
+++ schoolreport/models.py
@@ -9,9 +9,12 @@ class UserProfile(models.Model):
 
 class School(models.Model):
 	emis = models.IntegerField(unique = True)
+	province = models.CharField(max_length=2)
 	school_status = models.CharField(max_length=100)
 	name = models.CharField(max_length=100)
 	school_type = models.CharField(max_length=100)
+	school_sector = models.CharField(max_length=100)
+	specialisation = models.CharField(max_length=100)
 	section21 = models.BooleanField()
 	fee = models.IntegerField()
 	municipality = models.CharField(max_length=100)
@@ -22,8 +25,12 @@ class School(models.Model):
 	principal = models.CharField(max_length = 30)
 	physical_address = models.CharField(max_length = 255)
 	district = models.CharField(max_length=100)
-	
-
+	circuit = models.IntegerField()
+	students = models.IntegerField()
+	teachers = models.IntegerField()
+	passrate_2009 = models.IntegerField()
+	passrate_2010 = models.IntegerField()
+	passrate_2011 = models.IntegerField()
 
 def create_profile(sender, instance, created, **kwargs):
     if created:
