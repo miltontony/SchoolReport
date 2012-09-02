8b9c7f6ec10bf0036726761a9c549d1ac944648a
Milton Madanda <milton@praekeltfoundation.org>
2012-09-02 16:15:14 +0200

    show address in school dropdown

--- schoolreport/admin.py
+++ schoolreport/admin.py
@@ -5,5 +5,10 @@ from schoolreport.models import *
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'school')
 
+
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ('name', 'emis', 'physical_address', 'school_sector')
+    ordering = ('name',)
+
 admin.site.register(UserProfile, UserProfileAdmin)
-admin.site.register(School)
+admin.site.register(School, SchoolAdmin)
